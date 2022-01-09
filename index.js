4ed1df0d22cd095fd9f4d2748709520da0faba96
Adam Muzzarelli <admuzzarelli@gmail.com>
2022-01-08 19:18:24 -0800

    cleaned up code

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const Web3 = require('web3');
-const support = require('./support');
+const {get_latest_block_num} = require('./support');
+
+
 
 const web3 = new Web3('https://cloudflare-eth.com');
 
-support.get_latest_block_num(web3);
+get_latest_block_num(web3);
--- support.js
+++ support.js
@@ -5,9 +5,17 @@ const Big = require('bignumber.js');
 // Support modules used as helper functions for dev //
 //////////////////////////////////////////////////////
 
+
+
 //simply gets latest block # off eth chain
 const get_latest_block_num = async (web) => {
 	const latest_num = await web.eth.getBlockNumber();
 	console.log(latest_num);
 }
-exports.get_latest_block_num = get_latest_block_num;
+
+
+
+//export list
+module.exports = {
+	get_latest_block_num,
+};
