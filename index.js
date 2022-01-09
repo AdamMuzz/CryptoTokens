e2d921a236672d4ec786008497dc8246ee40fb97
Adam Muzzarelli <admuzzarelli@gmail.com>
2022-01-08 19:00:54 -0800

    modularized functions

--- index.js
+++ index.js
@@ -1,10 +1,6 @@
 const Web3 = require('web3');
+const support = require('./support');
 
 const web3 = new Web3('https://cloudflare-eth.com');
 
-const get_latest_block_num = async () => {
-	const latest_num = await web3.eth.getBlockNumber();
-	console.log(latest_num);
-}
-
-get_latest_block_num();
+support.get_latest_block_num(web3);
---
+++ support.js
@@ -0,0 +1,13 @@
+const Web3 = require('web3');
+const Big = require('bignumber.js');
+
+//////////////////////////////////////////////////////
+// Support modules used as helper functions for dev //
+//////////////////////////////////////////////////////
+
+//simply gets latest block # off eth chain
+const get_latest_block_num = async (web) => {
+	const latest_num = await web.eth.getBlockNumber();
+	console.log(latest_num);
+}
+exports.get_latest_block_num = get_latest_block_num;
